769e5c0eda4ecc34e528739b3674b6005d7dbdcd
Stefan Roggensack <someone561@gmx.de>
2013-09-20 21:46:37 +0200

    Result from tvdb sometimes over 1 MB (f.e. The Simpsons) => Move to a Datastore Cache with only required Data

--- tvcal.py
+++ tvcal.py
@@ -2,44 +2,79 @@ import tvdb_api
 import datetime
 from icalendar import Calendar, Event
 import urllib2
-from GMemcache import GMemcache
 import webapp2
 import json
+import logging
 from DatastoreCache import DataStoreCache
 from google.appengine.ext import db
 
 apikey='DCDC02D859CD26EF'
 
+class CalendarEntry(db.Model):
+    # Three List to simulate a list of trippel
+    summaries = db.StringListProperty()
+    dates = db.StringListProperty()
+    uids = db.StringListProperty()
+    created = db.DateTimeProperty(required=True)
+
 class Tvcal(webapp2.RequestHandler):
+    maxAge = 21600
+    
     def get(self, sids):
         self.response.headers['Content-Type'] = 'text/calendar'
         self.response.out.write(self.getCalendar(sids.split(',')))
         
     def getCalendar(self, sids):
-        tvdb = tvdb_api.Tvdb(cache=urllib2.build_opener(GMemcache, DataStoreCache), apikey=apikey)
+        tvdb = tvdb_api.Tvdb(cache=False, apikey=apikey)
         cal = Calendar()
         cal.add('prodid', '-//tvcal//mxm.dk//')
         cal.add('version', '2.0')    
-    
-        for sid in sids:
-            serie = tvdb[int(sid)]
-            [cal.add_component(self.createEvent(episode, serie['seriesname'])) for season in serie.values() 
-                for episode in season.values() 
-                    if episode['firstaired']]
+        
+        entries = [self.getCache(tvdb, sid) for sid in sids]
+        for e in entries:
+            [cal.add_component(self.createEvent(*details)) for details in zip(e.uids, e.summaries, e.dates)]
         return cal.to_ical()
     
-    def createEvent(self, episode, serie):
+    def createEvent(self, uid, summary, date):
         event = Event()
-        event.add('uid', episode["id"])
-        event.add('summary', '%s S%02dE%02d %s' % (serie, int(episode['seasonnumber']), int(episode['episodenumber']), episode['episodename']))
-        event.add('dtend', datetime.datetime.strptime(episode['firstaired'], "%Y-%m-%d").date())
-        event.add('dtstart', datetime.datetime.strptime(episode['firstaired'], "%Y-%m-%d").date())
+        event.add('uid', uid)
+        event.add('summary', summary)
+        dateString = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        event.add('dtend', dateString)
+        event.add('dtstart', dateString)
         return event
+        
+    def createCalendarEntry(self, serie):
+        cache = CalendarEntry(key_name=serie['id'],
+                      created=datetime.datetime.now())
+        for season in serie.values():
+            for episode in season.values():
+                if (episode['firstaired']):
+                    cache.summaries.append('%s S%02dE%02d %s' % (serie['seriesname'], int(episode['seasonnumber']), int(episode['episodenumber']), episode['episodename']))
+                    cache.dates.append(episode['firstaired'])
+                    cache.uids.append(episode['id'])
+        return cache
+    
+    def getCache(self, tvdb, sid):
+        cache = CalendarEntry.get_by_key_name(sid)
+        if (cache is not None):
+            logging.info("Datastore Cache hit")
+            if (datetime.datetime.now() - cache.created < datetime.timedelta(seconds=self.maxAge)):
+                return cache
+            logging.info("But Cache is to old")
+            cache.delete()
+        logging.info("Query tvdb")
+        cache = self.createCalendarEntry(tvdb[int(sid)])
+        cache.put()
+        return cache
+        
+    def getDetails(self, episode, serie):
+        return ('%s S%02dE%02d %s' % (serie, int(episode['seasonnumber']), int(episode['episodenumber']), episode['episodename']), episode['firstaired']) 
 
 class Search(webapp2.RequestHandler):
     def get(self, search):
         self.response.headers['Content-Type'] = 'application/json'
-        tvdb = tvdb_api.Tvdb(cache=urllib2.build_opener(GMemcache, DataStoreCache), apikey=apikey)
+        tvdb = tvdb_api.Tvdb(cache=urllib2.build_opener(DataStoreCache), apikey=apikey)
         self.response.out.write(json.dumps(tvdb.search(search)))
 
 class Banner(db.Model):
